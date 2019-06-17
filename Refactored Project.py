8ecf8501bd24b27acbc8101c0d8b425c0e4be684
gmandonnan <47415809+gabedonnan@users.noreply.github.com>
2019-06-17 11:28:27 +0100

    Update Refactored Project.py
    Added ability to end turns, added more cards

--- Refactored Project.py
+++ Refactored Project.py
@@ -44,7 +44,11 @@ class Player:
             (self.playerTwoBoard).remove(card)
             
     def endTurn(self, player):
-        pass
+        for card in playerOneBoard:
+            card.executeFunction(card.endFunc, 1)
+        for card in playerTwoBoard:
+            card.executeFunction(card.endFunc, 2)
+        #add a global player changer, possibly as a class variable
 
     def play(self, mana, cardPos, player):
         if player == 1:
@@ -64,15 +68,16 @@ class Player:
                 self.playerTwoBoard.append((self.playerTwoHand).pop(cardPos))
                 card_played.executeFunction(card_played.playedFunc,2)
                 mana -= card_played.mana
-                print("You played " + card_played.name + ", costing you " + str(card_played.mana) + " mana")
+                print("You played a card, costing you " + str(card_played.mana) + " mana")
             else:
-                print("You failed to play " + card_played.name + ", it costs " + str(card_played.mana) + " mana, whereas you have only " + str(mana) + " mana.")
+                print("You failed to play a card, it costs " + str(card_played.mana) + " mana, whereas you have only " + str(mana) + " mana.")
             print("You have " + str(mana) + " mana remaining.")
             return mana
 class Card:
-    def __init__(self,mana, attack, health, playedFunc = "pass", destroyedFunc = "pass", attackFunc = "pass", endFunc = "pass"):
+    def __init__(self,name, mana, attack, health, playedFunc = "pass", destroyedFunc = "pass", attackFunc = "pass", endFunc = "pass"):
         #for all the func ones input block of text which is passed into generic functions containing only an exec block, this saves me from having to write hundreds of new functions and allows for creations of new cards extremely quickly
         #The function text defaults to a function that does nothing
+        self.name = name
         self.playedFunc = playedFunc
         self.destroyedFunc = destroyedFunc
         self.attackFunc = attackFunc
@@ -84,19 +89,32 @@ class Card:
     def executeFunction(self,text, playerNum):
         exec(text)
 
-ragnaros = Card(8,2,8,playedFunc = """if playerNum == 1:
-    for i in player.playerTwoHand:
+cards = []
+cards.append(Card("Ragnaros the Firelord" ,8,2,8,playedFunc = """if playerNum == 1:
+    for i in player.playerTwoBoard:
         i.health -= 8
+        if i.health <= 0:
+            player.destroy(i,2)
 else:
-    for i in player.playerOneHand:
-        i.health -= 8""")
-sylvannas = Card(6,5,5, destroyedFunc = """if playerNum == 1 and player.playerTwoBoard:
+    for i in player.playerOneBoard:
+        i.health -= 8
+        if i.health <= 0:
+            player.destroy(i,1)"""))
+        
+cards.append(Card("Sylvannas windrunner",6,5,5, destroyedFunc = """if playerNum == 1 and player.playerTwoBoard:
     (player.playerOneBoard).append((player.playerTwoBoard).pop(random.randint(0,len(player.playerTwoBoard)-1)))
 elif player.playerOneBoard and playerNum == 2:
-    player.playerTwoBoard.append(random.choice(player.playerOneBoard).pop)""")
-
-oger = Card(6,6,7)
-player = Player([ragnaros, oger, sylvannas],[])
+    player.playerTwoBoard.append(random.choice(player.playerOneBoard).pop)"""))
+cards.append(Card("Emperor Thaurissan",6,5,5, endFunc = """if playerNum == 1:
+    for i in player.playerOneHand:
+        i.mana -= 1
+else:
+    for i in player.playerTwoHand:
+        i.mana -= 1"""))
+cards.append(Card("Bolderfist Oger",6,6,7))
+player = Player([cards[1],cards[2],cards[3]],[cards[0]])
 #For any variable "player" or "playerNum" within a function this refers to the player *in control* of the thing making the effect, not necessarily the player being affected
+player.draw(100,1)
+player.draw(100,2)
 
     
